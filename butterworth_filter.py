726664b30fb0a33a55a2b3315ded4bd121f79c37
Alexandr-star <sanya-bass.skvortzoff@yandex.ru>
2020-03-01 15:13:41 +0300

    buttfilter create

--- butterworth_filter.py
+++ butterworth_filter.py
@@ -1,7 +1,43 @@
-class ButterworthFiler:
+import numpy
+from scipy import signal
 
-    def __init__(self):
-        pass
+#from scipy import signal
 
-    def buttFilter(self):
-        pass
+class ButterworthFilter:
+    
+    def __init__(self, orderFiltr,
+                    criticalFrequency,
+                    samplingFrequency,
+                    typeFiltr="lowpass",
+                    analog=True):
+        # Порядок фильтра
+        self.__orderFiltr = orderFiltr 
+        # Критическая чачтота или частоты 
+        self.__criticalFrequency = criticalFrequency
+        # тип фильтра
+        self.__typeFiltr = typeFiltr 
+        # Аналоговый фильтр
+        self.__analog = analog
+        # Частота дискретизации цифровой системы.
+        self.__samplingFrequency = samplingFrequency
+
+    def buttFilter(self, sample):
+        sos = signal.butter(self.__orderFiltr,
+                            self.__criticalFrequency,
+                            btype=self.__typeFiltr,
+                            analog=self.__analog,
+                            output="sos",
+                            fs=self.__samplingFrequency)
+        filteredSample = signal.sosfilt(sos, sample)
+        
+        return filteredSample
+
+    def AFRfilter(self):
+        b, a = signal.butter(self.__orderFiltr,
+                             self.__criticalFrequency,
+                             btype=self.__typeFiltr, 
+                             analog=self.__analog, 
+                             output="ba")
+        w, h = signal.freqs(b, a)
+        
+        return w, h
--- sample.py
+++ sample.py
@@ -8,4 +8,5 @@ class Sample:
     def filtering(self):
         pass
 
-    
+    def drawChart(self):
+        pass
